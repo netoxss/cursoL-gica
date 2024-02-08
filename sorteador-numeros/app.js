940263230cbafb43639e38bc698e9572eb37613d
netoxss <netop0227@gmail.com>
2024-02-08 13:44:38 -0400

    Primeiros códigos

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,13 +1,49 @@
-let sorteio;
+let quantidade;
+let deNum;
+let ateNum;
 
-function Sorteio(de, ate, qtd) {
+function InicioGame() {
 
-    for (i = 0; i < qtd; i++)
-    {
-        sorteio = Math.random() * (ate - de) +de
-        console.log(sorteio)
-    }
-    
+    document.getElementById("btn-reiniciar").removeAttribute("disabled")
+    document.getElementById("btn-sortear").removeAttribute("disabled")
+
 }
 
-Sorteio(1, 5, 2)
+//Busca a quantidade que o usuário deseja no input definido por id no parâmetro
+function Quantidade(){
+    valor = document.querySelector("#quantidade");
+    quantidade = (valor.value);
+    return quantidade;
+}
+
+function DeNum() {
+    valor = document.querySelector("#de");
+    deNum = (valor.value);
+}
+
+function AteNum(){
+    valor = document.querySelector("#ate");
+    ateNum = (valor.value);
+    return ateNum;
+}
+
+function Sortear() {
+    let numA 
+
+    Quantidade();
+    DeNum();
+    AteNum();
+
+    for (i = 0; i < quantidade; i++) {
+    numA = (Math.random() * (ateNum - deNum)) + 1;
+    console.log(numA);
+    }
+
+}
+
+//Função para alterar HTML com os números sorteados
+function InnerHTML() {
+    document.InnerHTML("#resultado", "teste")
+}
+
+InicioGame();
