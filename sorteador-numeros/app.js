ef423d70dd1b6951a915ad0d1b4d3b8c9f26e110
netoxss <netop0227@gmail.com>
2024-02-09 10:03:34 -0400

    Adição de InnerHTML, e sistema de sorteio finalizado

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,13 +1,16 @@
 let quantidade;
 let deNum;
 let ateNum;
+let numA = [];
 
 function InicioGame() {
 
-    document.getElementById("btn-reiniciar").removeAttribute("disabled")
-    document.getElementById("btn-sortear").removeAttribute("disabled")
+    
+    InnerHTML("Números sorteados: nenhum até agora");
 
 }
+
+// InicioGame();
 
 //Busca a quantidade que o usuário deseja no input definido por id no parâmetro
 function Quantidade(){
@@ -16,34 +19,50 @@ function Quantidade(){
     return quantidade;
 }
 
+//Busca o inicio que o usuário deseja no input definido por id no parâmetro
 function DeNum() {
     valor = document.querySelector("#de");
     deNum = (valor.value);
 }
 
+//Busca o fim que o usuário deseja no input definido por id no parâmetro
 function AteNum(){
     valor = document.querySelector("#ate");
     ateNum = (valor.value);
     return ateNum;
 }
 
+//Função para alterar HTML com os números sorteados
+function InnerHTML(texto) {
+
+    let query = document.querySelector("#resultado");
+    return query.innerHTML = texto;
+}
+
+//Sortea os números de acordo com os valores que o usuário deseja
 function Sortear() {
-    let numA 
 
     Quantidade();
     DeNum();
     AteNum();
 
+    let num;
+
     for (i = 0; i < quantidade; i++) {
-    numA = (Math.random() * (ateNum - deNum)) + 1;
-    console.log(numA);
+    num = (Math.random() * (ateNum - deNum)) + 1.5;
+    numA.push(num);
     }
-
+    InnerHTML(`Números sorteados: ${numA}`);
+    document.getElementById("btn-reiniciar").setAttribute("enabled", "disabled")
 }
 
-//Função para alterar HTML com os números sorteados
-function InnerHTML() {
-    document.InnerHTML("#resultado", "teste")
+function Reiniciar() {
+    InicioGame();
+    numA = [];
 }
 
-InicioGame();
+
+
+
+
+
