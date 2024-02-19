e81abdca8d0a82a0bad5fa14d48b9946ad1231b0
netoxss <netop0227@gmail.com>
2024-02-19 11:21:27 -0400

    Refatoração de código e validação de entrada dos inputs de limite

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,6 +1,6 @@
-let quantidade;
-let deNum;
-let ateNum;
+let quantidade = 0;
+let deNum = 0;
+let ateNum= 0;
 let numA = [];
 
 function InicioGame() {
@@ -8,29 +8,17 @@ function InicioGame() {
     InnerHTML("Números sorteados: nenhum até agora");
     document.getElementById("resultado").setAttribute("class", "texto__paragrafo")
     numA = [];
-    //document.getElementById("#resultado").className.replace("", "texto__paragrafo")
 }
 
 InicioGame();
 
 //Busca a quantidade que o usuário deseja no input definido por id no parâmetro
-function Quantidade(){
-    valor = document.querySelector("#quantidade");
-    quantidade = (valor.value);
-    return quantidade;
-}
+function GetValues(){
+    quantidade = document.querySelector("#quantidade").value;
+    deNum = document.querySelector("#de").value;
+    ateNum = document.querySelector("#ate").value;
 
-//Busca o inicio que o usuário deseja no input definido por id no parâmetro
-function DeNum() {
-    valor = document.querySelector("#de");
-    deNum = (valor.value);
-}
-
-//Busca o fim que o usuário deseja no input definido por id no parâmetro
-function AteNum(){
-    valor = document.querySelector("#ate");
-    ateNum = (valor.value);
-    return ateNum;
+    return quantidade, deNum, ateNum;
 }
 
 //Função para alterar HTML com os números sorteados
@@ -40,22 +28,40 @@ function InnerHTML(texto) {
     return query.innerHTML = texto;
 }
 
-//Sortea os números de acordo com os valores que o usuário deseja
+//Sorteia os números de acordo com os valores que o usuário deseja
 function Sortear() {
 
-    Quantidade();
-    DeNum();
-    AteNum();
-
-    let num;
-
-    for (i = 0; i < quantidade; i++) {
-    num = (Math.random() * (ateNum - deNum)) + 1.5;
-    numA.push(parseInt(num));
+    GetValues();
+    
+    if (deNum > ateNum) {
+        InnerHTML("Número inicial deve ser menor que o final! Entrada inválida.");
     }
-    InnerHTML(`Números sorteados: ${numA}`);
-    document.getElementById("resultado").setAttribute("class", "texto__paragrafo");
-    AtivaBotao("btn-reiniciar", "btn-sortear");
+    else {
+        let num;
+
+        for (i = 0; i < quantidade; i++) {
+            num = (Math.random() * (ateNum - deNum)) + 1.5;
+            numA.push(parseInt(num));
+        }
+        InnerHTML(`Números sorteados: ${numA}`);
+        document.getElementById("resultado").setAttribute("class", "texto__paragrafo");
+        AtivaBotao("btn-reiniciar", "btn-sortear");
+    }
+
+    //     let num;
+
+    // for (i = 0; i < quantidade; i++) {
+    // num = (Math.random() * (ateNum - deNum)) + 1.5;
+    // numA.push(parseInt(num));
+    // }
+    // InnerHTML(`Números sorteados: ${numA}`);
+    // document.getElementById("resultado").setAttribute("class", "texto__paragrafo");
+    // AtivaBotao("btn-reiniciar", "btn-sortear");
+
+    // InnerHTML("Número inicial maior que o final, entrada inválida!");
+    // setTimeout(5);
+    // InicioGame();
+    
 }
 
 function Reiniciar() {
@@ -68,13 +74,6 @@ function AtivaBotao(ativa, desativa) {
     document.getElementById(desativa).setAttribute("class", "container__botao-desabilitado");
     document.getElementById(ativa).setAttribute("class", "container__botao");
 }
-
-// function AtivaSorteio() {
-//         document.getElementById("btn-sortear").removeAttribute("disabled");
-//         document.getElementById("btn-reiniciar").setAttribute("disabled", "");
-//         document.getElementById("btn-reiniciar").setAttribute("class", "container__botao-desabilitado");
-//         document.getElementById("btn-sortear").setAttribute("class", "container__botao");
-// }
 
 
 
